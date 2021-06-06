0a096ff3c855be92efb506f62c0ed4c51e8846a9
sandra-35 <84616320+sandra-35@users.noreply.github.com>
2021-06-06 15:35:09 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Footer() {
+  var currentyear = new Date().getFullYear();
+  return (
+    <footer>
+      <p> copyright @{currentyear} </p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and ReactJs workshop </h1>
+
+      <p>
+        This was a wonderful workshop and was really beneficial. Thanking shape
+        AI and the metor Shaurya Sinha for arranging this for us.
+      </p>
+    </div>
+  );
+}
+export default Note;
